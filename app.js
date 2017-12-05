0b83f7a3eed2f3c6a98b842476d7e8dc287e93c9
ALFA <hichamlh@hotmail.com>
2017-12-04 20:20:49 -0500

    make it better

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/doc', doc);
 app.use('/installations', installations);
 app.use('/getNomInstallation', getNomInstallation);
 app.use('/installationParNom', installationParNom);
-app.use('/installationMauvaise', conditionInstallation);
+app.use('/installationInstallation', conditionInstallation);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
